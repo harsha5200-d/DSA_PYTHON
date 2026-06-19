62e91ff06fe800b16f41f84362369bb738754d01
harsha <hreshavena@gmail.com>
2026-06-19 15:49:56 +0530

    trees nooofleafs1

---
+++ TREES/NoofNonLeaf.py
@@ -0,0 +1,27 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+
+
+def NoofLeaf(root):
+    if root is None:
+        return 0
+
+    if root.left is None and root.right is None:
+        return 0
+
+    return 1+ NoofLeaf(root.left) + NoofLeaf(root.right)
+
+
+# Create tree
+root = Node(1)
+root.left = Node(2)
+root.right = Node(3)
+root.left.left = Node(4)
+root.left.right = Node(5)
+root.right.right = Node(6)
+
+# Count leaf nodes
+print("Number of leaf nodes:", NoofLeaf(root))
